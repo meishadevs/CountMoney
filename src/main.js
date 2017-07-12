3630643eb11afe0037851063f0e9d5b31e0a741e
meishadevs <meishadevs@163.com>
2017-07-12 12:08:48 +0800

    使用axios与服务器通信

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import axios from 'axios';
+import qs from 'qs';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$http = axios;
+Vue.prototype.qs = qs;
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -6,7 +6,7 @@
     <p class="notice-title">请输入 您的信用卡号后8位</p>
 
     <!-- 信用卡号输入框 s -->
-    <input type="text" class="card-number" maxlength="8" v-model="cardNum">
+    <input type="text" class="card-number" maxlength="8" v-model="cardNum" @click="isShowNotice=false">
     <!-- 信用卡号输入框 e -->
 
     <!-- 注册信用卡账号按钮 s -->
@@ -14,7 +14,7 @@
     <!-- 注册信用卡账号按钮 e -->
 
     <!-- 输入错误产生的提示信息 s -->
-    <p class="error-info">您输入的信用卡号后八位不正确，请重新输入</p>
+    <p class="error-info" v-if="isShowNotice">您输入的信用卡号后八位不正确，请重新输入！</p>
     <!-- 输入错误产生的提示信息 e -->
 
     <p class="notice-content">
@@ -31,30 +31,69 @@
     data: function () {
       return {
 
-        // 用于记录用户输入的信用卡号后8位
+        //用于记录用户输入的信用卡号后8位
         cardNum: '',
 
-        // 信用卡账号输入错误产生的提示信息
-        noticeInfo: ''
+        //标记是否显示信用卡号后8位输入错误时产生的提示信息
+        isShowNotice: false
       };
     },
 
     methods: {
 
-      // 注册
+      //注册
       rigisterCardNum: function () {
 
-        // 检测
-        this.checkCardNum();
+          if (this.checkCardNum()) {
+
+               //将信用卡号保存在本地
+              localStorage.setItem('cardNum', this.cardNum);
+
+              //使用axios发送post请求实现注册
+              this.$http({
+                method: 'post',
+                url: 'http://localhost/zhsqServer/register.php',
+                data: this.qs.stringify({cardnum: this.cardNum}),
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                }
+              }).then(function(res){
+                console.log(res.data);
+              });
+          }
       },
 
-      // 检测信用卡账号是否合法
+      //检测信用卡账号是否合法
       checkCardNum: function () {
 
-        // 如果用户没有输入信用卡账号
-        if (this.cardNum.length == 0) {
-          console.log('调用');
+        //如果用户输入的信用卡号不足8位
+        if(this.cardNum.length < 8) {
+
+          //显示信用卡号输入错误的提示信息
+          this.isShowNotice = true;
+
+          return false;
         }
+
+        //如果输入的信用卡号后8位中存在汉字
+        if(/[\u4E00-\u9FA5]/g.test(this.cardNum)) {
+
+          //显示信用卡号输入错误的提示信息
+          this.isShowNotice = true;
+
+          return false;
+        }
+
+        //如果玩家输入的信用卡号后8位中存在非数字的字符
+        if(!(/^\d+$/.test(this.cardNum)))
+        {
+          //显示信用卡号输入错误的提示信息
+          this.isShowNotice = true;
+
+          return false;
+        }
+
+        return true;
       }
     }
   };
