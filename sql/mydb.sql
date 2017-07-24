b058f48a57b84733458863c7ce30abe004860ee7
meishadevs <meishadevs@163.com>
2017-07-24 16:30:47 +0800

    操作数据库的SQL语句

---
+++ sql/mydb.sql
@@ -0,0 +1,23 @@
+﻿SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS  `account`;
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cardnum` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+DROP TABLE IF EXISTS  `gameinfo`;
+CREATE TABLE `gameinfo` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `cardnum` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `nummoney` int(11) DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `month` int(11) DEFAULT NULL,
+  `day` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
